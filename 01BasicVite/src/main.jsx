18792f9dd5feaea658bb5ccf11eaf2b6a76e8676
kalp_1210 <rambhiakalp@gmail.com>
2024-08-14 02:04:52 +0530

    react baiscs with chai aur code

--- 01BasicVite/src/main.jsx
+++ 01BasicVite/src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Children, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import React from "react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// this is a object and isme joh props wagera hai woh alag kuch define hai in JSON toh this will not work kyu ki yeh react ke tree main convert nahi ho pata hai
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   Children: "Click me to visit google",
+// };
+
+// This below wala object is defined by tree structure toh yeh chalta hai
+
+// const anotherElement = (
+//   <a href="https://google.com" target="_blank">
+//     Visit google
+//   </a>
+// );
+
+// const ReactElement = React.createElement(
+//   "a",
+//   { href: "https://google.com", target: "_blank" },
+//   "click me to visit google"
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
