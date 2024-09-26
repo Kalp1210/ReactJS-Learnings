a069783883ce2dfeaf5cb99b7a3b45ed3e9d7c45
kalp_1210 <rambhiakalp@gmail.com>
2024-09-26 20:03:36 +0530

    .

--- MegaReactProject01/src/appwrite/auth.js
+++ MegaReactProject01/src/appwrite/auth.js
@@ -11,6 +11,32 @@ export class AuthService {
     this.client.setEndpoint(appwriteUrl).setProject(appwriteProjectId);
     this.account = new Account(this.client);
   }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        // call another method
+        // like sign up ke baad wapas login karana hai ya phir direct app ke andar daal na hai
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login(email, password) {
+    try {
+      await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 const authService = new AuthService();
