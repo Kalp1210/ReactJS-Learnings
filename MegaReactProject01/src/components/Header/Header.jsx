53b19049986963e97569ee0518efdb6c10b3e9a6
kalp_1210 <rambhiakalp@gmail.com>
2024-09-30 22:07:28 +0530

    nothing

--- MegaReactProject01/src/components/Header/Header.jsx
+++ MegaReactProject01/src/components/Header/Header.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { Container, Logo, LogoutBtn } from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+
+  const naItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: auth,
+    },
+  ];
+
+  return (
+    <div>
+      <header className=""></header>
+    </div>
+  );
 };
 
 export default Header;
--- MegaReactProject01/src/components/Header/LogoutBtn.jsx
+++ MegaReactProject01/src/components/Header/LogoutBtn.jsx
@@ -6,9 +6,17 @@ import { logout } from "../../store/authSlice";
 const LogoutBtn = () => {
   const dispatch = useDispatch();
   const logoutHandler = () => {
-    authService.logout();
+    authService.logout().then(() => {
+      dispatch(logout());
+    });
   };
-  return <div></div>;
+  return (
+    <div>
+      <button className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default LogoutBtn;
