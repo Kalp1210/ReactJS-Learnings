2010c5d016f2d2357621abe9f151cb6e3cc1ba25
kalp_1210 <rambhiakalp@gmail.com>
2024-10-01 11:20:17 +0530

    postcard,login,select components

--- MegaReactProject01/src/components/Select.jsx
+++ MegaReactProject01/src/components/Select.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useId } from "react";
 
-const Select = () => {
-  return <div>Select</div>;
+const Select = ({ options, label, className = "", ...props }, ref) => {
+  const id = useId();
+  return (
+    <div>
+      <div className="w-full">
+        {label && <label htmlFor={id} className=""></label>}
+        <select
+          {...props}
+          id={id}
+          ref={ref}
+          className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        >
+          {options?.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
 };
 
-export default Select;
+export default React.forwardRef(Select);
